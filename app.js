dbbc23b85d74635cf0a71bd8e0c393262076a13a
hp-book <solares.josue@outlook.com>
2022-01-05 21:19:19 -0600

    Add morgan to project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000 || process.env.PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Api online" });
